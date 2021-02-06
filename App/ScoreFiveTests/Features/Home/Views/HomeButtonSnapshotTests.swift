0e436204b75a6409b59cf6371a258f09f997e098
Varun Santhanam <talkto@vsanthanam.com>
2021-02-06 15:53:51 -0800

    [HomeButton] Add Snapshot Tests
    Summary: Add snapshot tests for `HomeButton`
    
    Test Plan: tests only, no logic changes
    
    Differential Revision: https://phab.vsanthanam.com/D137

---
+++ App/ScoreFiveTests/Features/Home/Views/HomeButtonSnapshotTests.swift
@@ -0,0 +1,47 @@
+//
+//  HomeButtonSnapshotTests.swift
+//  ScoreFiveTests
+//
+//  Created by Varun Santhanam on 2/6/21.
+//
+
+@testable import ScoreFive
+import FBSnapshotTestCase
+import SnapKit
+
+final class HomeButtonSnapshotTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+    }
+    
+    func test_home_button() {
+        let canvas = UIView(frame: .init(origin: .init(x: 0.0,
+                                                       y: 0.0),
+                                         size: .init(width: 300.0,
+                                                     height: 300.0)))
+        let button = HomeButton(title: "Home Button")
+        canvas.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.center.equalToSuperview()
+        }
+        FBSnapshotVerifyView(canvas)
+    }
+    
+    func test_home_button_highlighted() {
+        let canvas = UIView(frame: .init(origin: .init(x: 0.0,
+                                                       y: 0.0),
+                                         size: .init(width: 300.0,
+                                                     height: 300.0)))
+        let button = HomeButton(title: "Home Button")
+        canvas.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.center.equalToSuperview()
+        }
+        button.isHighlighted = true
+        FBSnapshotVerifyView(canvas)
+    }
+}
